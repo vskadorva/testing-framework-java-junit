e5a79444af5e31850ecd8e4846f3e57876f9e937
Vitaly Skadorva <vitaly@Vitalys-MacBook-Pro.local>
2023-03-27 15:15:34 -0400

    my first test

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,11 +1,34 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FirstSeleniumTest {
+    private static final String HOME_PAGE_URL = "https://www.facebook.com/";
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void classSetup() {
+        driver = SharedDriver.getWebdriver();
+        driver.get(HOME_PAGE_URL);
+    }
+
+    @AfterAll
+    public static void classTearDown() {
+        SharedDriver.closeDriver();
+    }
+
+    @AfterEach
+    public void testTearDown() {
+        driver.get(HOME_PAGE_URL);
+    }
 
     @Test
-    public void openBrowserTest() {
-      WebDriver driver = SharedDriver.getWebdriver();
-      SharedDriver.closeDriver();
+    public void homePageUrlTest() {
+        String actual_URL = driver.getCurrentUrl();
+        assertEquals(HOME_PAGE_URL, actual_URL, "The URL doesn't match");
     }
 }
